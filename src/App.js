7370bc913552dc16ad3aca3a92b1e165b735dfa9
Mikael Petersen <info@mikaelpetersen.se>
2020-03-31 15:13:03 +0200

    added search function and added video to ui

--- src/App.js
+++ src/App.js
@@ -3,13 +3,41 @@ import Header from "./Header";
 import UserHint from "./UserHint";
 
 class App extends React.Component {
-  constructor(prop) {
-    super(prop);
+  constructor(props) {
+    super(props);
     this.state = {
       searchTerm: "",
-      hintText: ""
+      hintText: "",
+      gif: null
     };
   }
+
+  // We want a function that searches the Giphy API using fetch and puts the search term into the query url and then we can do something with the results
+  // We can also write async methods into our components that lets us the async/await style of function
+  searchGiphy = async searchTerm => {
+    const API_KEY = "6p7rYjivDzeZL9ke1T2jA5MFuswaRZqP";
+
+    const randomChoice = arr => {
+      const randIndex = Math.floor(Math.random() * arr.length);
+      return arr[randIndex];
+    };
+
+    try {
+      const response = await fetch(
+        `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchTerm}&limit=50&offset=0&rating=PG-13&lang=en`
+      );
+      const { data } = await response.json();
+
+      // Here we grab a random result from our images
+      const randomGif = randomChoice(data);
+
+      this.setState((prevState, props) => ({
+        ...prevState,
+        gif: randomGif
+      }));
+      console.log(data.data);
+    } catch (error) {}
+  };
 
   handleChange = event => {
     const { value } = event.target;
@@ -28,19 +56,29 @@ class App extends React.Component {
     const { value } = event.target;
     // When we have 2 or more characters in our search box and we have also pressed enterl, we then want to run a search
     if (value.length > 2 && event.key === "Enter") {
-      alert(`search for ${value}`);
+      // Here we call our searchGiphy function using the search term
+      this.searchGiphy(value);
     }
     console.log(event.key);
   };
 
   render() {
-    const { searchTerm } = this.state;
+    const { searchTerm, gif } = this.state;
 
     return (
       <div className="page">
         <Header />
         <div className="search grid">
           {/* Our stack of gif images */}
+          {/* It's only going to render out video when we have a gif in the state, we can test for it using && */}
+          {gif && (
+            <video
+              className="grid-item video"
+              autoPlay
+              loop
+              src={gif.images.original.mp4}
+            />
+          )}
           <input
             className="input grid-item"
             placeholder="Type something"
